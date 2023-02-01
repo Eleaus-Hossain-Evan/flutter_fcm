e8b5c8a3ec49e5ffad4c46e5e5780b83f4e6fd1a
Eleaus-Hossain-Evan <eleaus16-364@diu.edu.bd>
2023-02-01 14:41:52 +0600

    chnage on signup

--- lib/screen/sign_up_screen.dart
+++ lib/screen/sign_up_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easylogger/flutter_logger.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends HookConsumerWidget {
   static const route = "/sign_up";
   const SignUpScreen({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    useEffect(() {
+      Logger.d("SignUpScreen");
+    });
     return const Scaffold(
       body: Center(
         child: Text(
